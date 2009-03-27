25d8fe6edc07baefbd39453b212fb22a9fb8c192
Victor Costan <victor@moonstone.local>
2009-03-27 14:38:24 -0400

    More failure resilience in process management.

--- lib/daemonz/process.rb
+++ lib/daemonz/process.rb
@@ -1,34 +1,48 @@
-begin
-  # use the sys-proctable gem if it's available
-  if RUBY_PLATFORM =~ /darwin/
-    raise 'Need to mock sys-proctable because it fails on OSX.'
-  else
-    require 'sys/proctable'
-  end
-rescue Exception
-  # mock it using ps if it's not available
-  
-  module Sys
+# Mocks the sys-proctable gem using ps.
+#
+# This is useful even if sys-proctable is available, because it may fail for
+# random reasons.
+module Daemonz::ProcTable
+  class ProcInfo
+    def initialize(pid, cmdline)
+      @pid = pid
+      @cmdline = cmdline
+    end
+    attr_reader :pid, :cmdline
   end
 
-  module Sys::ProcTable
-    class ProcInfo
-      def initialize(pid, cmdline)
-        @pid = pid
-        @cmdline = cmdline
-      end
-      attr_reader :pid, :cmdline
+  def self.ps_emulation
+    retval = []
+    ps_output = `ps ax`
+    ps_output.each_line do |pline|
+      pdata = pline.split(nil, 5)
+      pinfo = ProcInfo.new(pdata[0].strip, pdata[4].strip)
+      retval << pinfo
     end
-  
+    return retval
+  end
+end
+
+begin
+  require 'sys/proctable'
+    
+  module Daemonz::ProcTable
     def self.ps
-      retval = []
-      ps_output = `ps ax`
-      ps_output.each_line do |pline|
-        pdata = pline.split(nil, 5)
-        pinfo = ProcInfo.new(pdata[0].strip, pdata[4].strip)
-        retval << pinfo
+      # We don't use ps_emulation all the time because sys-proctable is
+      # faster. We only pay the performance penalty when sys-proctable fails.
+      begin
+        Sys::ProcTable.ps
+      rescue Exception
+        self.ps_emulation
       end
-      return retval
+    end
+  end
+rescue Exception
+  # The accelerated version is not available, use the slow version all the time.
+  
+  module Daemonz::ProcTable
+    def self.ps
+      self.ps_emulation
     end
   end
 end
@@ -37,7 +51,8 @@ module Daemonz
   # returns information about a process or all the running processes
   def self.process_info(pid = nil)
     info = Hash.new
-    Sys::ProcTable.ps.each do |process|
+    
+    Daemonz::ProcTable.ps.each do |process|
       item = { :cmdline => process.cmdline, :pid => process.pid.to_s }
 
       if pid.nil?
@@ -46,6 +61,7 @@ module Daemonz
         return item if item[:pid].to_s == pid.to_s
       end
     end
+    
     if pid.nil?
       return info
     else
